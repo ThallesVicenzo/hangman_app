44287166e30e59796ecd269962910e949c495a99
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-10-11 14:53:22 -0300

    home screen updated to pass the api data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hangman_app/screens/game_screen.dart';
 import '../routes/named_routes.dart';
+import '../services/hangman-model.dart';
 import '../widgets/rounded_button.dart';
 import '../widgets/text_widget.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  var hangmanDataFromApi;
+
+  @override
+  initState() {
+    super.initState();
+    getDataFromService();
+  }
+
+  Future<dynamic> getDataFromService() async {
+    hangmanDataFromApi = await HangmanModel().createGame();
+    return hangmanDataFromApi;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,27 +36,37 @@ class HomeScreen extends StatelessWidget {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              TextWidget(title: 'HANGMAN', fontSize: 50,),
+              TextWidget(
+                title: 'HANGMAN',
+                fontSize: 50,
+              ),
               const Image(
                 image: AssetImage('assets/images/gallow.png'),
               ),
               Column(
                 children: [
                   RoundedButton(
+                    borderRadius: BorderRadius.circular(10.0),
                     color: Colors.blue,
                     title: 'Start',
                     onPressed: () {
-                      Navigator.pushNamed(
-                        context,
-                        NamedRoutes.game,
-                      );
+                      print(hangmanDataFromApi);
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return GameScreen(
+                          hangmanDataFromApi
+                        );
+                      }));
                     },
-                    style: const TextStyle(fontFamily: 'PatrickHand', color: Colors.white, fontSize: 25),
+                    style: const TextStyle(
+                        fontFamily: 'PatrickHand',
+                        color: Colors.white,
+                        fontSize: 25),
                   ),
                   const SizedBox(
                     height: 20,
                   ),
                   RoundedButton(
+                    borderRadius: BorderRadius.circular(10.0),
                     color: Colors.blue,
                     title: 'High Scores',
                     onPressed: () {
@@ -45,7 +75,10 @@ class HomeScreen extends StatelessWidget {
                         NamedRoutes.highscores,
                       );
                     },
-                    style: const TextStyle(fontFamily: 'PatrickHand', color: Colors.white, fontSize: 25),
+                    style: const TextStyle(
+                        fontFamily: 'PatrickHand',
+                        color: Colors.white,
+                        fontSize: 25),
                   ),
                 ],
               )
