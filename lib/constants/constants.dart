d4b90ca7f5e94cda9f1de6e3cac8af3843941fdd
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-10-13 10:40:08 -0300

    constants created and updated

---
+++ lib/constants/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kLogo = 'assets/images/gallow.png';
+const kHang = 'assets/images/0.png';
+const kHangHead = 'assets/images/1.png';
+const kHangHeadChest = 'assets/images/2.png';
+const kHangHeadArm1 = 'assets/images/3.png';
+const kHangHeadArm2 = 'assets/images/4.png';
+const kHangHeadLeg1 = 'assets/images/5.png';
+const kHangHeadLeg2 = 'assets/images/6.png';
+
+const kBackgroundColor = Color(0xff421b9c);
+
+const kTextButtonStyle = TextStyle(
+fontFamily: 'PatrickHand',
+color: Colors.white,
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+);
