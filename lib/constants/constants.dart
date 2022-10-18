5d4d61d2d8a4f0a04d16c8ec7ba72e54347833f0
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-10-18 14:33:51 -0300

    convert keyboard list to dynamic

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -9,6 +9,35 @@ const kHangHeadArm2 = 'assets/images/4.png';
 const kHangHeadLeg1 = 'assets/images/5.png';
 const kHangHeadLeg2 = 'assets/images/6.png';
 
+const kKeyboard = <dynamic> [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
 const kBackgroundColor = Color(0xff421b9c);
 
 const kTextButtonStyle = TextStyle(
