e0df56fc0f25b09ab3c12b4019993678883c61b9
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-10-18 14:34:34 -0300

    guess letter request created

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -4,19 +4,35 @@ import 'dart:convert';
 
 class HangmanApiService {
   HangmanApiService(this.url);
+
   final String url;
 
   Future hangmanRequest(String url) async {
     http.Response response = await http.post(Uri.parse(url));
-      try {
-        if (response.statusCode == 200) {
-          var data = jsonDecode(response.body);
-          return data;
-        } else {
-          print(response.statusCode);
-        }
-      } catch(e){
-        print(e);
+    try {
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        return data;
+      } else {
+        print(response.statusCode);
       }
+    } catch (e) {
+      print(e);
     }
   }
+
+  Future hangmanLetter(String url) async {
+    http.Response response = await http.put(Uri.parse(url));
+    try {
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        print(response.body);
+        return data;
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
