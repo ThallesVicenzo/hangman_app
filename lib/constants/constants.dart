721430240a1db83df5495511ddf9008cd4e8ecf1
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-11-04 14:29:26 -0300

    kTextField decoration added

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -47,5 +47,21 @@ const kTextButtonStyle = TextStyle(
 fontFamily: 'PatrickHand',
 color: Colors.white,
   fontSize: 25,
-  fontWeight: FontWeight.bold,
+);
+
+const kTextFieldDecoration = InputDecoration(
+  hintStyle: kTextButtonStyle,
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 3.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 3.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
 );
