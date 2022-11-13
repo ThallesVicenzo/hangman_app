c63c41657105bb67b818951e296ab28c70900e96
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-11-13 15:12:30 -0300

    sign out service created

--- lib/screens/game_screens/game_home_screen.dart
+++ lib/screens/game_screens/game_home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hangman_app/screens/authenticate_screens/auth_home_screen.dart';
 import 'package:hangman_app/screens/game_screens/game_screen.dart';
 import '../../constants/constants.dart';
 import '../../routes/named_routes.dart';
@@ -16,6 +18,16 @@ class GameHomeScreen extends StatefulWidget {
 
 class _GameHomeScreenState extends State<GameHomeScreen> {
 
+  final _auth = FirebaseAuth.instance;
+
+  signOut() async {
+    await _auth.signOut();
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) {
+          return AuthHomeScreen(widget.hangmanData);
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,14 +36,61 @@ class _GameHomeScreenState extends State<GameHomeScreen> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            TextWidget(
-              title: 'HANGMAN',
-              fontSize: 50,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: (){
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: kBackgroundColor,
+                            title: TextWidget(
+                              title: 'Are you sure that you want to logout?',
+                              fontSize: 25,
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  signOut();
+                                },
+                                child: TextWidget(
+                                  title: 'Yes',
+                                  fontSize: 30,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: TextWidget(
+                                  title: 'No',
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  child: Image(
+                    image: AssetImage(kLogout),
+                    color: Colors.white,
+                  ),
+                ),
+                TextWidget(
+                  title: 'HANGMAN',
+                  fontSize: 50,
+                ),
+                Visibility(
+                  visible: false,
+                  child: TextWidget(title: 'placeHolder', fontSize: 1),
+                ),
+              ],
             ),
             Image(
               image: AssetImage(kHangmanGallow),
             ),
-
             CustomTextButton(
                 width: 130,
                 label: 'Start',
