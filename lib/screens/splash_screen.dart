087786c81b36d4ffc5d75e919e9ae97737a2db38
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-11-21 03:30:42 -0300

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:hangman_app/constants/constants.dart';
 import 'package:hangman_app/screens/authenticate_screens/auth_home_screen.dart';
 import 'package:hangman_app/screens/game_screens/game_home_screen.dart';
+=======
+>>>>>>> parent of f0ffc33 (splash screen code refactored)
 
 import '../services/hangman_api/hangman-model.dart';
 
@@ -18,7 +21,20 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   initState() {
     super.initState();
+<<<<<<< HEAD
     getDataFromService();
+=======
+    Timer(
+      Duration(milliseconds: 2500),
+      () {
+
+        setState ((){
+          opacity = 0;
+        });
+
+      },
+    );
+>>>>>>> parent of f0ffc33 (splash screen code refactored)
   }
 
   final _auth = FirebaseAuth.instance;
@@ -54,6 +70,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< HEAD
       backgroundColor: kBackgroundColor,
       body: AnimatedOpacity(
         duration: kThemeAnimationDuration,
@@ -65,6 +82,15 @@ class _SplashScreenState extends State<SplashScreen> {
           child: Image.asset('assets/images/gallow.png'),
         ),
       ),
+=======
+      backgroundColor: const Color(0xff421b9c),
+      body: AnimatedOpacity(duration: kThemeAnimationDuration, opacity: opacity, onEnd: (){
+        if(opacity == 0){
+          Navigator.of(context).pushReplacementNamed(NamedRoutes.home);
+        }
+      },
+      child: Center(child: Image.asset('assets/images/gallow.png'),),),
+>>>>>>> parent of f0ffc33 (splash screen code refactored)
     );
   }
 }
