21719a74f85e1d5a4af1e421ae26e0cce5d7de88
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-11-22 23:38:23 -0300

    fixes when user press back button routes

--- lib/screens/authenticate_screens/auth_home_screen.dart
+++ lib/screens/authenticate_screens/auth_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hangman_app/routes/named_routes.dart';
 import 'package:hangman_app/screens/authenticate_screens/login_screen.dart';
 import 'package:hangman_app/screens/authenticate_screens/sign_up_screen.dart';
@@ -15,40 +16,74 @@ class _AuthHomeScreenState extends State<AuthHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: kBackgroundColor,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            TextWidget(
-              title: 'HANGMAN',
-              fontSize: 50,
-            ),
-            Image(
-              image: AssetImage(kHangmanGallow),
-            ),
-            CustomTextButton(
-                width: 130,
-                label: 'Login',
-                fontSize: 20,
-                onPressed: () {
-                  Navigator.pushNamed(context, NamedRoutes.login);
-                }),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomTextButton(
-                  label: 'SignUp',
+      child: WillPopScope(
+        onWillPop: () async {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  backgroundColor: kBackgroundColor,
+                  title: TextWidget(
+                    title: 'Are you sure you want to close this app?',
+                    fontSize: 30,
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          SystemNavigator.pop();
+                        },
+                        child: TextWidget(
+                          title: 'Yes',
+                          fontSize: 25,
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: TextWidget(
+                          title: 'No',
+                          fontSize: 25,
+                        )),
+                  ],
+                );
+              });
+          return true;
+        },
+        child: Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextWidget(
+                title: 'HANGMAN',
+                fontSize: 50,
+              ),
+              Image(
+                image: AssetImage(kHangmanGallow),
+              ),
+              CustomTextButton(
+                  width: 130,
+                  label: 'Login',
                   fontSize: 20,
                   onPressed: () {
-                    Navigator.pushNamed(context, NamedRoutes.signUp);
-                  },
-                ),
-              ],
-            )
-          ],
+                    Navigator.pushNamed(context, NamedRoutes.login);
+                  }),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextButton(
+                    label: 'SignUp',
+                    fontSize: 20,
+                    onPressed: () {
+                      Navigator.pushNamed(context, NamedRoutes.signUp);
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/game_screens/game_home_screen.dart
+++ lib/screens/game_screens/game_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hangman_app/screens/authenticate_screens/auth_home_screen.dart';
 import 'package:hangman_app/screens/game_screens/game_screen.dart';
 import '../../constants/constants.dart';
@@ -40,96 +41,131 @@ class _GameHomeScreenState extends State<GameHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: kBackgroundColor,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            backgroundColor: kBackgroundColor,
-                            title: TextWidget(
-                              title: 'Are you sure that you want to logout?',
-                              fontSize: 25,
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  signOut();
-                                },
-                                child: TextWidget(
-                                  title: 'Yes',
-                                  fontSize: 30,
-                                ),
+      child: WillPopScope(
+        onWillPop: () async {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  backgroundColor: kBackgroundColor,
+                  title: TextWidget(
+                    title: 'Are you sure you want to close this app?',
+                    fontSize: 30,
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          SystemNavigator.pop();
+                        },
+                        child: TextWidget(
+                          title: 'Yes',
+                          fontSize: 25,
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: TextWidget(
+                          title: 'No',
+                          fontSize: 25,
+                        )),
+                  ],
+                );
+              });
+          return true;
+        },
+        child: Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              backgroundColor: kBackgroundColor,
+                              title: TextWidget(
+                                title: 'Are you sure that you want to logout?',
+                                fontSize: 25,
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: TextWidget(
-                                  title: 'No',
-                                  fontSize: 30,
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    signOut();
+                                  },
+                                  child: TextWidget(
+                                    title: 'Yes',
+                                    fontSize: 30,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(
-                      image: AssetImage(kLogout),
-                      color: Colors.white,
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: TextWidget(
+                                    title: 'No',
+                                    fontSize: 30,
+                                  ),
+                                ),
+                              ],
+                            );
+                          });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage(kLogout),
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                TextWidget(
-                  title: 'HANGMAN',
-                  fontSize: 50,
-                ),
-                Visibility(
-                  visible: false,
-                  child: TextWidget(title: 'placeHolder', fontSize: 1),
-                ),
-              ],
-            ),
-            Image(
-              image: AssetImage(kHangmanGallow),
-            ),
-            CustomTextButton(
-                width: 130,
-                label: 'Start',
-                fontSize: 20,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return GameScreen(hangmanDataFromApi: hangmanString);
-                  }));
-                }),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomTextButton(
-                  label: 'High Scores',
+                  TextWidget(
+                    title: 'HANGMAN',
+                    fontSize: 50,
+                  ),
+                  Visibility(
+                    visible: false,
+                    child: TextWidget(title: 'placeHolder', fontSize: 1),
+                  ),
+                ],
+              ),
+              Image(
+                image: AssetImage(kHangmanGallow),
+              ),
+              CustomTextButton(
+                  width: 130,
+                  label: 'Start',
                   fontSize: 20,
                   onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      NamedRoutes.highscores,
-                    );
-                  },
-                ),
-              ],
-            )
-          ],
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return GameScreen(hangmanDataFromApi: hangmanString,);
+                    }));
+                  }),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextButton(
+                    label: 'High Scores',
+                    fontSize: 20,
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        NamedRoutes.highscores,
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
