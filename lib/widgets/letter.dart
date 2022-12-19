953dd4ceddc6f862a4a7265bfb2697417e068216
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-12-19 16:01:25 -0300

    text fixed

--- lib/widgets/letter.dart
+++ lib/widgets/letter.dart
@@ -5,17 +5,18 @@ Widget Letter(String character, bool hidden) {
   return Container(
     height: 65,
     width: 50,
-    padding: EdgeInsets.all(12.0),
     decoration: BoxDecoration(
-      color: Colors.lightBlue,
+      color: Colors.black45,
       borderRadius: BorderRadius.circular(4.0),
     ),
     child: Visibility(
       visible: !hidden,
-      child: TextWidget(
-        title: character,
-        fontSize: 40,
-        fontWeight: FontWeight.bold,
+      child: Center(
+        child: TextWidget(
+          title: character,
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     ),
   );
