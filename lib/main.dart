c66d76e14edf820edf8ca701232dd95807545e86
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-12-22 15:28:00 -0300

    shared preferences removal

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hangman_app/routes/named_routes.dart';
 import 'package:hangman_app/routes/routes.dart';
-import 'package:hangman_app/services/shared_preferences/storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await StorageService.init();
   await Firebase.initializeApp();
   runApp(HangmanApp());
 }
